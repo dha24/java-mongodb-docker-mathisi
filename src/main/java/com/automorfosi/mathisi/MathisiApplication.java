49216ddd8443b2025a69c90801736cd768ebcc9c
dharmendra kumar <ddharmendr@gmail.com>
2023-06-05 09:45:15 +0530

    removed default connection to MongoDB if not using it.

--- src/main/java/com/automorfosi/mathisi/MathisiApplication.java
+++ src/main/java/com/automorfosi/mathisi/MathisiApplication.java
@@ -1,9 +1,19 @@
 package com.automorfosi.mathisi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+
+
+/**
+ * Spring Boot tries to automatically configure a MongoDB connection if it detects the presence of the MongoDB driver on the classpath
+ *  To disable the automatic MongoDB configuration in your Spring Boot application, you can use the @EnableAutoConfiguration annotation with the exclude attribute
+ */
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class MathisiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MathisiApplication.class, args);
