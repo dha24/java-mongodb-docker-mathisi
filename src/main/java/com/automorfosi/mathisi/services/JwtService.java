48150d6b4f3d67f5fea24f4088498f35534d8eac
dharmendra kumar <ddharmendr@gmail.com>
2023-06-20 13:47:13 +0530

    Implemented token generation logic: 1. JwtService : responsible for all the logic related to the token 2. UserService : Logic to manage user activities

---
+++ src/main/java/com/automorfosi/mathisi/services/JwtService.java
@@ -0,0 +1,47 @@
+package com.automorfosi.mathisi.services;
+
+import com.automorfosi.mathisi.models.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JwtService {
+    private static final String SECRET_KEY = "25432AA@@!!!OINJA~SKET~54@#$56A586E3L876FMESTD2723575387(8^D#LO82F413F4{,}";
+    //todo Implement KMS
+
+    private Key getSignInKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateToken(User userInfo){
+        HashMap<String, String> extraClaims = new HashMap<String, String>(); //todo all extra claims where you can update other required claims.
+        extraClaims.put("orgId", userInfo.getOrgId());
+        extraClaims.put("orgName", userInfo.getOrgName());
+        extraClaims.put("userRole", userInfo.getRole().name()); //todo Make this role as an array
+        return generateToken(extraClaims, userInfo);
+    }
+
+    public String generateToken(
+            Map<String, String> extraClaims,
+            User userInfo
+    ){
+        return Jwts.
+                builder().
+                setClaims(extraClaims).
+                setSubject(userInfo.getEmail()).
+                setIssuedAt(new Date(System.currentTimeMillis())).
+                setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 2 )). //todo currently 2 hr, change it to the 30 mins
+                        signWith(getSignInKey(), SignatureAlgorithm.HS256).
+                compact();
+    }
+
+
+
+}
--- src/main/java/com/automorfosi/mathisi/services/UserService.java
+++ src/main/java/com/automorfosi/mathisi/services/UserService.java
@@ -1,22 +1,56 @@
 package com.automorfosi.mathisi.services;
 
+import com.automorfosi.mathisi.models.AuthToken;
+import com.automorfosi.mathisi.models.JwtToken;
 import com.automorfosi.mathisi.models.User;
 import com.automorfosi.mathisi.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
+
+    private final PasswordEncoder passwordEncoder;
+
     private final UserRepository userRepository;
+
+    private final AuthenticationManager authenticationManager;
+
+    private final JwtService jwtService;
+
 
     //adjust the page and size parameters according to your need
     public Page<User> getEnabledUsers(int page, int size) {
+        logger.debug("fetching enabled users");
         Pageable pageable = PageRequest.of(page, size);
         return userRepository.findAllByEnabledTrue(pageable);
     }
+
+    public AuthToken authenticate(JwtToken jwtTokenRequest){
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        jwtTokenRequest.getEmail(),
+                        jwtTokenRequest.getPassword()
+                )
+        );
+
+        var user = userRepository.findByEmail(jwtTokenRequest.getEmail())
+                .orElseThrow();
+        var jwtToken  = jwtService.generateToken(user);
+        //todo implement logic for the refresh token
+        var message  = "Token Generated!!";
+        return AuthToken.builder().jwtToken(jwtToken).build();
+    }
+
 
 }
